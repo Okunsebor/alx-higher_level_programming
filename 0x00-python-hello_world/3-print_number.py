3e262c4bcb867795c66f1a2ec1e6c7c58ae63df5
Okunsebor <joelosehioria@gmail.com>
2023-03-08 23:54:27 +0000

    Updated

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 number = 98
-print(f"{number:d} Battery street") #Explicitly indicate what type of formatted string "{number}" iswith the aid of ":d"
+print(f"{number:d} Battery street")
