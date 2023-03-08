7a3c11067a4b3a34e7383390a0951c3fa3f0112e
Okunsebor <joelosehioria@gmail.com>
2023-03-08 02:17:16 +0000

    Advanced fixed

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(26):
-    if i % 2 == 0:
-        print('{:c}'.format(122 - i), end='')
-    else:
-        print('{:c}'.format(90 - i), end='')
+for i in range(ord('z'), ord('a')-1, -1):
+	if i % 2 == 0:
+		print("{:c}".format(i), end='')
+	else:
+		print("{:c}".format(i-32), end='')
